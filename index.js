d8cedd0b9ac5c12eeab60dd3fbf65abb36e45de2
Filipe <lipeqa@gmail>
2022-05-12 11:30:25 -0300

    primeiro commit- adiciona projeto do pokapi

---
+++ index.js
@@ -0,0 +1,56 @@
+/*$("#btn-pokemon").on("click", function(){    
+    let nomePokemon = $("#pokemon").val();
+    let site = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`
+    $.ajax({
+        method: "GET",
+        url: site,
+        dataType: "json",
+        success:function(result){
+            console.log(result);
+            $("#nome-pokemon").html(`Nome: ${result.name}  <br> ID:  ${result.id}`)
+            $("#habilidade-pokemon").html(`Movimeto: ${result.moves[0].move.name} <br> Habilidade: ${result.abilities[0].ability.name}`)
+            $("#img-pokemon").html(`<img src="${result.sprites.front_default}">`)
+        }
+      });  
+})*/
+
+let btnPokemom = document.querySelector("#btn-pokemon");
+let nome = document.querySelector("#nome-pokemon");
+let id = document.querySelector("#id-pokemon");
+let movimentoPokemon = document.querySelector("#movimento-pokemon");
+let habilidadePokemom = document.querySelector("#habilidade-pokemon");
+let imgPokemom = document.querySelector("#img-pokemon");
+
+btnPokemom.addEventListener('click', function(e){
+    e.preventDefault();
+    let nomePokemon = document.querySelector('#pokemon').value.toLowerCase();
+    let req = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`;
+
+    requisicao(req);
+})
+
+function requisicao(urls){ // faz a requisição
+    $.ajax({
+        method: "GET",
+        url: urls,
+        dataType: "json",
+
+        success:function(result){
+            //console.log(result);
+            buscaObjeto(result);
+        }
+      });
+}
+function buscaObjeto(result){    //faz a busca das propriedades do personagem
+    nome.textContent =`Nome: ${result.name} `;
+    id.textContent =`ID:  ${result.id}` ;   
+    movimentoPokemon.textContent = ` Movimeto: ${result.moves[0].move.name}`;
+    habilidadePokemom.textContent = `Habilidade: ${result.abilities[0].ability.name}`;
+
+    let img = document.createElement("img");
+     img.src = `${result.sprites.front_default}`;
+     img.style.width = "100%";
+     img.style.height = "100%";
+     document.querySelector('#img-pokemon').appendChild(img);
+
+}
