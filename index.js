71af32a3ec70ace47ec228a3e603ec1d3620dfc5
Filipe <lipeqa@gmail>
2022-05-13 13:54:34 -0300

    atera arquivo css

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-/*$("#btn-pokemon").on("click", function(){    
-    let nomePokemon = $("#pokemon").val();
-    let site = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`
-    $.ajax({
-        method: "GET",
-        url: site,
-        dataType: "json",
-        success:function(result){
-            console.log(result);
-            $("#nome-pokemon").html(`Nome: ${result.name}  <br> ID:  ${result.id}`)
-            $("#habilidade-pokemon").html(`Movimeto: ${result.moves[0].move.name} <br> Habilidade: ${result.abilities[0].ability.name}`)
-            $("#img-pokemon").html(`<img src="${result.sprites.front_default}">`)
-        }
-      });  
-})*/
 let btnPokemom = document.querySelector("#btn-pokemon");
 let nome = document.querySelector("#nome-pokemon");
 let id = document.querySelector("#id-pokemon");
@@ -36,21 +21,36 @@ function requisicao(urls){ // faz a requisição
         dataType: "json",
 
         success:function(result){
-            //console.log(result);
+           // console.log(result);
             buscaObjeto(result);
         }
       });
 }
-
 function buscaObjeto(result){    //faz a busca das propriedades do personagem
     nome.textContent =`Nome: ${result.name} `;
     id.textContent =`ID:  ${result.id}` ;   
     movimentoPokemon.textContent = `Movimeto: ${result.moves[0].move.name}`;
     habilidadePokemom.textContent = `Habilidade: ${result.abilities[0].ability.name}`;
-
-    
-     img.src = `${result.sprites.front_default}`;
-     img.style.width = "100%";
-     img.style.height = "100%";
-     imgPokemom.appendChild(img);
+    img.src = `${result.sprites.front_default}`;
+    imgPokemom.appendChild(img);
 }
+
+
+
+
+
+/*$("#btn-pokemon").on("click", function(){    
+    let nomePokemon = $("#pokemon").val();
+    let site = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`
+    $.ajax({
+        method: "GET",
+        url: site,
+        dataType: "json",
+        success:function(result){
+            console.log(result);
+            $("#nome-pokemon").html(`Nome: ${result.name}  <br> ID:  ${result.id}`)
+            $("#habilidade-pokemon").html(`Movimeto: ${result.moves[0].move.name} <br> Habilidade: ${result.abilities[0].ability.name}`)
+            $("#img-pokemon").html(`<img src="${result.sprites.front_default}">`)
+        }
+      });  
+})*/
